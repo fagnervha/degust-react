f888cac70ef33be6e6f48d68d048af861fba79d1
fagnervha <fagner.oliveira.ro@gmail.com>
2024-08-12 09:35:58 -0400

    firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,13 +6,13 @@ importScripts(
 );
 // // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyC65OXItWgAtKfioVXWsGD2ReGFycUstR4",
+  authDomain: "degust-delivery-1fc62.firebaseapp.com",
+  projectId: "degust-delivery-1fc62",
+  storageBucket: "degust-delivery-1fc62.appspot.com",
+  messagingSenderId: "1087572610631",
+  appId: "1:1087572610631:web:de662c3500d6cd02b42b62",
+  measurementId: "G-GZHXX5WZV1",
 };
 
 firebase?.initializeApp(firebaseConfig);
--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import {
 } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyC65OXItWgAtKfioVXWsGD2ReGFycUstR4",
+  authDomain: "degust-delivery-1fc62.firebaseapp.com",
+  projectId: "degust-delivery-1fc62",
+  storageBucket: "degust-delivery-1fc62.appspot.com",
+  messagingSenderId: "1087572610631",
+  appId: "1:1087572610631:web:de662c3500d6cd02b42b62",
+  measurementId: "G-GZHXX5WZV1",
 };
 const firebaseApp = !getApps().length
   ? initializeApp(firebaseConfig)
@@ -32,7 +32,7 @@ const messaging = (async () => {
 
 export const fetchToken = async (setTokenFound, setFcmToken) => {
   return getToken(await messaging, {
-    vapidKey: "",
+    vapidKey: "BAuZBAvkBHpF44nnSZveCoGRQzM6sgheZlJI27GdhtPjk_G421NTwmxsfDH1b0D8eyl5tNYu3mekz3jGjfo35GQ",
   })
     .then((currentToken) => {
       if (currentToken) {
