9fa7492811e878d10ec95716abb575413a3b1334
fagnervha <fagner.oliveira.ro@gmail.com>
2024-08-12 11:12:53 -0400

    update idioma

--- src/language/i18n.js
+++ src/language/i18n.js
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import { portuguese } from './pt_BR'
 import { english } from "./en";
 import { bengali } from "./bn";
 import { arabic } from "./ar";
@@ -9,6 +10,9 @@ import { spain } from "./es";
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
+  pt: {
+    translation: portuguese,
+  },
   en: {
     translation: english,
   },
